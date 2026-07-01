6abb6561721ce332a2a49a90f89c48b07885a82a
swathi <battulaswathi15407@gmail.com>
2026-07-02 00:06:54 +0530

    terraform

--- ec2-01/main.tf
+++ ec2-01/main.tf
@@ -4,36 +4,36 @@ resource "aws_instance" "example"  {
     vpc_security_group_ids=[aws_security_group.allow_tls.id]
 
     tags = {
-        NAME = "terraform"
+        Name = "terraform"
         project = "roboshop"
     }
 
 }
 
-resource "aws_security_group "allow_tls"  {
+resource "aws_security_group"  "allow_tls"  {
     name = "allow-all-terraform"
-    description = "allow all  inbound and outbound traffic"
+    description = "allow all inbound and outbound traffic"
 
-
-    ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
 
     egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-    }
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 
     tags = {
-        NAME= "allow-all-terraform"
+        Name = "allow-all-terraform"
     }
 
 
